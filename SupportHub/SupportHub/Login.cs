944b4d78667865fc47ae3d5bfd47601ee36f85d7
SonIsai09 <102713688+SonIsai09@users.noreply.github.com>
2024-04-25 17:16:55 -0600

    Creación Login (#1)
    * Creacion login
    
    * Login agregado
    
    * Formulario de carga
    
    ---------
    
    Co-authored-by: IsaiAlvarezF <135726163+IsaiAlvarezF@users.noreply.github.com>

---
+++ SupportHub/SupportHub/Login.cs
@@ -0,0 +1,60 @@
+namespace SupportHub
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.FromArgb(31, 35, 40);
+
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+                txtUsuario.ForeColor = Color.FromArgb(31, 35, 40);
+            }
+        }
+
+        private void txtContraseña_Enter(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "CONTRASEÑA")
+            {
+                txtContraseña.Text = "";
+                txtContraseña.ForeColor = Color.FromArgb(31, 35, 40);
+                txtContraseña.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void txtContraseña_Leave(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "")
+            {
+                txtContraseña.Text = "CONTRASEÑA";
+                txtContraseña.ForeColor = Color.FromArgb(31, 35, 40);
+                txtContraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnCerrarLogin_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAcceder_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
