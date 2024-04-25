acba6d084403578598c253bb0b9af0b30fbc7e76
IsaiAlvarezF <135726163+IsaiAlvarezF@users.noreply.github.com>
2024-04-25 15:56:37 -0600

    Login agregado

--- SupportHub/SupportHub/Login.Designer.cs
+++ SupportHub/SupportHub/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SupportHub
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            pImagen = new Panel();
+            imgLogoLogin = new PictureBox();
+            lblLogin = new Label();
+            pUser = new Panel();
+            pPassword = new Panel();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            btnAcceder = new Button();
+            btnCerrarLogin = new FontAwesome.Sharp.IconButton();
+            pImagen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)imgLogoLogin).BeginInit();
+            SuspendLayout();
+            // 
+            // pImagen
+            // 
+            pImagen.BackColor = Color.FromArgb(3, 29, 52);
+            pImagen.Controls.Add(imgLogoLogin);
+            pImagen.Dock = DockStyle.Left;
+            pImagen.Location = new Point(0, 0);
+            pImagen.Name = "pImagen";
+            pImagen.Size = new Size(228, 330);
+            pImagen.TabIndex = 0;
+            // 
+            // imgLogoLogin
+            // 
+            imgLogoLogin.Image = (Image)resources.GetObject("imgLogoLogin.Image");
+            imgLogoLogin.Location = new Point(3, 3);
+            imgLogoLogin.Name = "imgLogoLogin";
+            imgLogoLogin.Size = new Size(227, 93);
+            imgLogoLogin.SizeMode = PictureBoxSizeMode.CenterImage;
+            imgLogoLogin.TabIndex = 0;
+            imgLogoLogin.TabStop = false;
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLogin.ForeColor = Color.FromArgb(31, 35, 40);
+            lblLogin.Location = new Point(458, 21);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(87, 32);
+            lblLogin.TabIndex = 0;
+            lblLogin.Text = "LOGIN";
+            // 
+            // pUser
+            // 
+            pUser.BackColor = Color.FromArgb(31, 35, 40);
+            pUser.Location = new Point(288, 106);
+            pUser.Name = "pUser";
+            pUser.Size = new Size(400, 3);
+            pUser.TabIndex = 0;
+            // 
+            // pPassword
+            // 
+            pPassword.BackColor = Color.FromArgb(31, 35, 40);
+            pPassword.Location = new Point(288, 196);
+            pPassword.Name = "pPassword";
+            pPassword.Size = new Size(400, 3);
+            pPassword.TabIndex = 0;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.BackColor = Color.FromArgb(148, 184, 216);
+            txtUsuario.BorderStyle = BorderStyle.None;
+            txtUsuario.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.ForeColor = Color.FromArgb(31, 35, 40);
+            txtUsuario.Location = new Point(288, 86);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(400, 20);
+            txtUsuario.TabIndex = 1;
+            txtUsuario.Text = "USUARIO";
+            txtUsuario.Enter += txtUsuario_Enter;
+            txtUsuario.Leave += txtUsuario_Leave;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.BackColor = Color.FromArgb(148, 184, 216);
+            txtContraseña.BorderStyle = BorderStyle.None;
+            txtContraseña.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtContraseña.ForeColor = Color.FromArgb(31, 35, 40);
+            txtContraseña.Location = new Point(288, 176);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(400, 20);
+            txtContraseña.TabIndex = 2;
+            txtContraseña.Text = "CONTRASEÑA";
+            txtContraseña.Enter += txtContraseña_Enter;
+            txtContraseña.Leave += txtContraseña_Leave;
+            // 
+            // btnAcceder
+            // 
+            btnAcceder.BackColor = Color.FromArgb(89, 134, 213);
+            btnAcceder.FlatAppearance.BorderSize = 0;
+            btnAcceder.FlatAppearance.MouseDownBackColor = Color.FromArgb(89, 134, 213);
+            btnAcceder.FlatAppearance.MouseOverBackColor = Color.FromArgb(3, 29, 52);
+            btnAcceder.FlatStyle = FlatStyle.Flat;
+            btnAcceder.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAcceder.ForeColor = Color.White;
+            btnAcceder.Location = new Point(288, 248);
+            btnAcceder.Name = "btnAcceder";
+            btnAcceder.Size = new Size(404, 43);
+            btnAcceder.TabIndex = 3;
+            btnAcceder.Text = "ACCERDER";
+            btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // btnCerrarLogin
+            // 
+            btnCerrarLogin.BackColor = Color.FromArgb(148, 184, 216);
+            btnCerrarLogin.FlatAppearance.BorderSize = 0;
+            btnCerrarLogin.FlatStyle = FlatStyle.Flat;
+            btnCerrarLogin.IconChar = FontAwesome.Sharp.IconChar.X;
+            btnCerrarLogin.IconColor = Color.FromArgb(31, 35, 40);
+            btnCerrarLogin.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            btnCerrarLogin.IconSize = 20;
+            btnCerrarLogin.Location = new Point(749, 3);
+            btnCerrarLogin.Name = "btnCerrarLogin";
+            btnCerrarLogin.Size = new Size(29, 23);
+            btnCerrarLogin.TabIndex = 4;
+            btnCerrarLogin.UseVisualStyleBackColor = false;
+            btnCerrarLogin.Click += btnCerrarLogin_Click;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(148, 184, 216);
+            ClientSize = new Size(780, 330);
+            Controls.Add(btnCerrarLogin);
+            Controls.Add(btnAcceder);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Controls.Add(pPassword);
+            Controls.Add(pUser);
+            Controls.Add(lblLogin);
+            Controls.Add(pImagen);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            pImagen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)imgLogoLogin).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel pImagen;
+        private PictureBox imgLogoLogin;
+        private Label lblLogin;
+        private Panel pUser;
+        private Panel pPassword;
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private Button btnAcceder;
+        private FontAwesome.Sharp.IconButton btnCerrarLogin;
     }
 }
--- SupportHub/SupportHub/Login.cs
+++ SupportHub/SupportHub/Login.cs
@@ -1,10 +1,55 @@
 namespace SupportHub
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "USUARIO")
+            {
+                txtUsuario.Text = "";
+                txtUsuario.ForeColor = Color.FromArgb(31, 35, 40);
+
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "USUARIO";
+                txtUsuario.ForeColor = Color.FromArgb(31, 35, 40);
+            }
+        }
+
+        private void txtContraseña_Enter(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "CONTRASEÑA")
+            {
+                txtContraseña.Text = "";
+                txtContraseña.ForeColor = Color.FromArgb(31, 35, 40);
+                txtContraseña.UseSystemPasswordChar = true;
+
+            }
+        }
+
+        private void txtContraseña_Leave(object sender, EventArgs e)
+        {
+            if (txtContraseña.Text == "")
+            {
+                txtContraseña.Text = "CONTRASEÑA";
+                txtContraseña.ForeColor = Color.FromArgb(31, 35, 40);
+                txtContraseña.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnCerrarLogin_Click(object sender, EventArgs e)
+        {
+           Application.Exit();
         }
     }
 }
