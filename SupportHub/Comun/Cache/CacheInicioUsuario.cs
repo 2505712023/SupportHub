3c28a6ab85b0b105a5485312bc4402b32ea396f9
IsaiAlvarezF <135726163+IsaiAlvarezF@users.noreply.github.com>
2024-05-07 17:01:08 -0600

    Mi perfil y cambiar contraseña

--- SupportHub/Comun/Cache/CacheInicioUsuario.cs
+++ SupportHub/Comun/Cache/CacheInicioUsuario.cs
@@ -9,7 +9,7 @@ namespace Comun.Cache
 {
     public static class CacheInicioUsuario
     {
-        public static int? IdUser { get; set; }
+        public static int IdUser { get; set; }
         public static string? password { get; set; }
         public static string? user { get; set;}   
         public static string? nombreUser { get; set; }
--- SupportHub/SupportHub/frmInformacion.cs
+++ SupportHub/SupportHub/frmInformacion.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Comun.Cache;
+using DataAccess;
 using Dominio;
 namespace Presentacion
 {
@@ -26,14 +27,108 @@ namespace Presentacion
         private void frmInformacion_Load(object sender, EventArgs e)
         {
             cargarDatoUsuario();
+            inicializarControlPass();
         }
-        private void cargarDatoUsuario() {
+        private void cargarDatoUsuario()
+        {
             lblIdInfo.Text = CacheInicioUsuario.IdUser.ToString();
-            lblUser.Text= CacheInicioUsuario.user;
+            lblUser.Text = CacheInicioUsuario.user;
             lblNombreInfo.Text = CacheInicioUsuario.nombreUser;
             lblApellidoInfor.Text = CacheInicioUsuario.apellidoUser;
 
+            txtUsuario.Text = CacheInicioUsuario.user;
+            txtNombreUsuario.Text = CacheInicioUsuario.nombreUser;
+            txtApellidoUsuario.Text = CacheInicioUsuario.apellidoUser;
+            txtNuevaContra.Text = CacheInicioUsuario.password;
+            txtContraActual.Text = "";
+            txtConfirmarContra.Text = CacheInicioUsuario.password;
+        }
+        private void inicializarControlPass()
+        {
 
+            lblEditar.Text = "Editar";
+            txtNuevaContra.Enabled = false;
+            txtNuevaContra.UseSystemPasswordChar = true;
+            txtConfirmarContra.Enabled = false;
+            txtConfirmarContra.UseSystemPasswordChar = true;
+            txtContraActual.UseSystemPasswordChar=true;
+        }
+
+        private void reset()
+        {
+            cargarDatoUsuario();
+            inicializarControlPass();
+
+        }
+
+        private void linkEditar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            pCambiarcontraseña.Visible = true;
+            cargarDatoUsuario();
+        }
+
+        private void lblEditar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (lblEditar.Text == "Editar")
+            {
+                lblEditar.Text = "Cancelar";
+                txtNuevaContra.Enabled = true;
+                txtNuevaContra.Text = "";
+                txtConfirmarContra.Enabled = true;
+                txtConfirmarContra.Text = "";
+               
+            }
+            else if (lblEditar.Text == "Cancelar")
+            {
+                reset();
+            }
+        }
+
+        private void btnGuardarContraseña_Click(object sender, EventArgs e)
+        {
+            string contraseñaActual = txtContraActual.Text;
+            string nuevaContraseña = txtNuevaContra.Text;
+            string confirmarContraseña = txtConfirmarContra.Text;
+
+            if (nuevaContraseña.Length >= 5)
+            {
+                if (nuevaContraseña == confirmarContraseña)
+                {
+                    if (contraseñaActual == CacheInicioUsuario.password)
+                    {
+                        ModeloUsuario modeloUsuario = new ModeloUsuario(CacheInicioUsuario.IdUser, nuevaContraseña);
+                        string resultado = modeloUsuario.EditarPerfil();
+                        MessageBox.Show(resultado);
+                        ResetearFormulario();
+                        pCambiarcontraseña.Visible = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contraseña actual incorrecta, por favor inténtalo de nuevo");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("La contraseña ingresada no coincide con la confirmación de contraseña, por favor inténtalo de nuevo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("La contraseña debe tener al menos 5 caracteres");
+            }
+
+
+        }
+        private void ResetearFormulario()
+        {
+            txtContraActual.Text = "";
+            txtNuevaContra.Text = "";
+            txtConfirmarContra.Text = "";
+        }
+        private void btnCancelaEmpleado_Click(object sender, EventArgs e)
+        {
+            pCambiarcontraseña.Visible=false;
+            reset();
         }
     }
 }
